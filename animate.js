4a06cd4247853bc3df4038bda0dd08875fa89b37
lin <ivlin112@gmail.com>
2016-04-30 11:48:05 -0400

    Supdated info

--- animate.js
+++ animate.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var balls=[]
 
 ctx.strokeStyle = "#000000";
 ctx.fillStyle = "#FF00FF";
@@ -8,71 +9,88 @@ ctx.strokeRect(0,0,canvas.width,canvas.height);
 ctx.stroke();
 ctx.closePath();
 
+
+var makeDot = function(startx,starty,startrad){
+    return {
+	x:startx,
+	y:starty,
+	xvel:5-Math.random()*10,
+	yvel:5-Math.random()*10,
+	radius:startrad,
+	mass:startrad*startrad,
+	collide:function(balls){
+	    if (this.x-this.radius <= 0 ||
+		this.x+this.radius >= canvas.width){
+		this.xvel*=-1;
+	    }
+	    if (this.y-this.radius <= 0 ||
+		this.y+this.radius >= canvas.height){
+		this.yvel*=-1;
+	    }
+	    var newxvel,newyvel;
+	    var massSum,massDifference;
+	    for (var i=0;i<balls.length;i++){
+		if (Math.pow(this.x-balls[i].x,2)+Math.pow(this.y-balls[i].y,2) < Math.pow(this.radius+balls[i].radius,2)){
+		    massSum=this.mass+balls[i].mass;
+		    massDifference=this.mass-balls[i].mass;
+		    newxvel = (this.xvel*(massDifference)+2*balls[i].mass*balls[i].xvel)/massSum;
+		    newyvel = (this.yvel*(massDifference)+2*balls[i].mass*balls[i].yvel)/massSum;
+		    balls[i].xvel = (balls[i].xvel*-massDifference+2*this.mass*this.xvel)/massSum;
+		    balls[i].yvel = (balls[i].yvel*-massDifference+2*this.mass*this.yvel)/massSum;;
+		    this.xvel = newxvel;
+		    this.yvel = newyvel;
+		}
+	    }
+	},
+	update:function(balls){
+	    this.collide(balls);
+	    this.x+=this.xvel;
+	    this.y+=this.yvel;
+	},
+	draw:function(){
+	    ctx.beginPath();
+	    ctx.strokeRect(0,0,canvas.width,canvas.height);
+	    ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
+	    ctx.stroke();
+	    ctx.fill();
+	    ctx.closePath();
+	},
+    }
+};
+
 var requestId;
-var logo = new Image();
-logo.src = "logo_dvd.jpg";
-var radius=0;
-var isGrowing=false;
-
-var drawDot = function drawDot(){
-    ctx.beginPath();
+var animate = function animate(){
     window.cancelAnimationFrame(requestId);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.strokeRect(0,0,canvas.width,canvas.height);
-    if (radius >= canvas.width/2 || radius <= 0){
-	isGrowing = !isGrowing;
+    ctx.fillStyle="#FFF";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle="#ABC";
+    for (var i=0;i<balls.length;i++){
+	balls[i].update(balls.slice(i+1,balls.length));
+	balls[i].draw();
     }
-    if (isGrowing) {radius+=1;}
-    else {
-	radius-=1;
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.strokeRect(0,0,canvas.width,canvas.height);
-	ctx.stroke();
-    } 
-   ctx.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
-    ctx.fill();
-    ctx.closePath();
-    requestId = window.requestAnimationFrame(drawDot);
-};
+    requestId = window.requestAnimationFrame(animate);
+}
 
-var dvd = function dvd(){
-    window.cancelAnimationFrame(requestId);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.strokeRect(0,0,canvas.width,canvas.height);
-    var xpos = 0;
-    var ypos = 0;
-    var xmag = 1;
-    var ymag = 1;
-    
-    var moveDvd = function moveDvd() {
-	if (xpos < 0 || xpos >= canvas.width - logo.width){
-	    xmag *= -1;
-	    ctx.strokeRect(0,0,canvas.width,canvas.height);
-	    //ctx.stroke();
-	}
-	if (ypos < 0 || ypos >= canvas.height - logo.height){
-	    ymag *= -1;
-	    ctx.strokeRect(0,0,canvas.width,canvas.height);
-	    //ctx.stroke();
-	}
-	xpos += xmag;
-	ypos += ymag;
-	ctx.drawImage(logo,xpos,ypos,logo.width,logo.height);
-	requestId = window.requestAnimationFrame(moveDvd);
+var setup = function setup(){
+    for (var i=0;i<document.getElementById("balls").value;i++){
+	var radius=Math.random()*20+20;
+	balls.push(makeDot(radius+Math.random()*(canvas.width-2*radius),
+			   radius+Math.random()*(canvas.height-2*radius),
+			   radius));
     }
-    moveDvd();
-};
+}
 
 var stop = function stop(){
     window.cancelAnimationFrame(requestId);
 };
 
-document.getElementById("start").addEventListener("click",drawDot);
-document.getElementById("dvd").addEventListener("click",function(){
-    var scaleFactor = canvas.width/logo.width/6;
-console.log(scaleFactor);
-     logo.width = logo.width * scaleFactor;
-     logo.height = logo.height * scaleFactor;
-    dvd();
-});
+var reset = function reset(){
+    balls=[];
+    ctx.fillStyle="#FFF";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+document.getElementById("start").addEventListener("click",setup);
+document.getElementById("animate").addEventListener("click",animate);
 document.getElementById("stop").addEventListener("click",stop);
+document.getElementById("reset").addEventListener("click",reset);
